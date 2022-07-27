92965fa3621c26a5184997ebd229308814732d94
Diego Ferreira L.G. Oliveira <diegoferreira1964@gmail.com>
2022-07-27 18:12:54 -0300

    Inserindo animacao com Lottie

--- App.tsx
+++ App.tsx
@@ -3,10 +3,12 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from './src/global/styles/theme';
 
+import { Welcome } from './src/pages/Welcome';
+
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-
+      <Welcome />
     </ThemeProvider>
   );
 }
